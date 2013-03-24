6f06c0a1ee52ebaa0dfa821fc6e6cae3f9e5df6c
Javi Filella <txusko@gmail.com>
2013-03-24 02:29:07 +0100

    Version 0.1
    New plugin features & Google analytics tracking

--- bookmarklet/postitall.js
+++ bookmarklet/postitall.js
@@ -76,15 +76,13 @@ if(typeof(Storage)!=="undefined") {
 		}
 		
 		function loadtracking() {
-			/*var _gaq = _gaq || [];
-				_gaq.push(['_setAccount', 'UA-35945028-2']);
-				_gaq.push(['_trackPageview']);
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-				(function() {
-					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-				})();*/
+			  ga('create', 'UA-39560817-1', 'googledrive.com');
+			  ga('send', 'pageview');
 		}
 
 		
